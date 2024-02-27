6ca562d39c1b6db182a5d1659d792b55316a2cb9
denefee <dsher2004@gmail.com>
2024-02-27 22:56:38 +0300

    vtk

---
+++ 2-vtk/CMakeFiles/udlin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/udlin.dir/udlin.cpp.o"
+  "CMakeFiles/udlin.dir/udlin.cpp.o.d"
+  "udlin"
+  "udlin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/udlin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2-vtk/CMakeFiles/udlin.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for udlin.
+# This may be replaced when dependencies are built.
